5cde0ecdc3ed6140f325d1760bd31638276902c8
Stephen Baker <stephen@endorsera.com>
2014-08-27 13:04:55 -0700

    Revert "pushing changes to merge-datalayer branch"
    This reverts commit b8050c95c93360695da0b5abc99d1b3b1d5f8a93.

---
+++ source/ddbb/data/dataContracts/IConnection.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ddbb.App.Data.DataContracts
+{
+    public interface IConnection
+    {
+		string Url { get; set; }
+
+		string Key { get; set; }
+
+		string Database { get; set; }
+    }
+}
---
+++ source/ddbb/data/documentDb/DocumentDbConnection.cs
@@ -0,0 +1,13 @@
+﻿using ddbb.App.Data.DataContracts;
+
+namespace ddbb.App.Data.DocumentDb
+{
+	public class DocumentDbConnection : IConnection
+	{
+		public string Url { get; set; }
+
+		public string Key { get; set; }
+
+		public string Database { get; set; }
+	}
+}
