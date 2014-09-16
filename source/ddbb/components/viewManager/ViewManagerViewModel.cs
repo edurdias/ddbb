971c482178c4cc5d724594a3ee414cf5de249b23
Eduardo Dias <edurdias@gmail.com>
2014-09-16 00:40:27 -0400

    adjusted references

--- source/ddbb/components/viewManager/ViewManagerViewModel.cs
+++ source/ddbb/components/viewManager/ViewManagerViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Specialized;
-using System.ComponentModel.Composition;
+﻿using System.ComponentModel.Composition;
 using Caliburn.Micro;
 using ddbb.App.Contracts.Events;
 using ddbb.App.Contracts.ViewModels;
